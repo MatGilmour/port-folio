bcdafd2f9c013a3ee2613dbb767bb66b6562c015
Mathieu Gilmour <mathieugilmour@gmail.com>
2023-08-22 00:37:01 -0400

    Adding NavBar to global layout

---
+++ port-folio/app/projects/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Portfolio of Projects',
+  description: 'Learn more about my skills, competencies and lifestyle',
+}
+
+export default function DashboardLayout({ children }) {
+  return <section>{children}</section>
+}
